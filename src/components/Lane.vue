11c14182d5f419549ec66f40d51e580a09b4a7e2
gusjohnson <enjoisf@gmail.com>
2019-11-17 14:44:32 -0600

    implementing lanes

--- src/components/Lane.vue
+++ src/components/Lane.vue
@@ -1,25 +1,59 @@
 <template>
   <div class="lane">
+    <h1>{{ name }}</h1>
+    <span class="add-link" v-if="addNewRestaurant === false" @click="addNewRestaurant = true">Add a restaurant</span>
+    <new-restaurant v-if="addNewRestaurant" @added="addRestaurant" @cancel="addNewRestaurant = false" />
+    <restaurant-card v-for="restaurant in restaurants" :key="restaurant.name" :restaurant="restaurant" @removed="removeRestaurant" />
   </div>
 </template>
 
 <script>
+import RestaurantCard from '@/components/RestaurantCard.vue'
+import NewRestaurant from '@/components/NewRestaurant.vue'
+
 export default {
-  name: 'Lane'
+  name: 'Lane',
+  components: {
+    RestaurantCard,
+    NewRestaurant
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      restaurants: [],
+      addNewRestaurant: false,
+    }
+  },
+  methods: {
+    addRestaurant(restaurant) {
+      this.restaurants.push({
+        ...restaurant,
+        lane: this.name,
+        displayOrder: this.restaurants.length
+      })
+      this.addNewRestaurant = false
+    },
+    removeRestaurant(removedRestaurant) {
+      this.restaurants = this.restaurants.filter(restaurant => restaurant.name !== removedRestaurant.name)
+    }
+  }
 }
 </script>
 
 <style scoped>
 .lane {
-  width: 15rem;
+  width: 20rem;
   height: 100%;
   border: 1px solid gray;
   border-radius: 0.5rem;
-  /* display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center; */
-  margin: 1rem;
-  flex: 1;
+}
+
+.add-link {
+  cursor: pointer;
 }
 </style>
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,37 +1,21 @@
 <template>
   <div class="board">
-    <!-- <span class="add-link" v-if="addNewRestaurant === false" @click="addNewRestaurant = true">Add a restaurant</span> -->
-    <lane />
-    <!-- <new-restaurant v-if="addNewRestaurant" @added="addRestaurant" @cancel="addNewRestaurant = false" />
-    <restaurant-card v-for="restaurant in restaurants" :key="restaurant.name" :restaurant="restaurant" @removed="removeRestaurant" /> -->
+    <lane v-for="lane in lanes" :key="lane" :name="lane" />
   </div>
 </template>
 
 <script>
-// import RestaurantCard from '@/components/RestaurantCard.vue'
-// import NewRestaurant from '@/components/NewRestaurant.vue'
 import Lane from '@/components/Lane.vue'
+import { lanes } from '@/constants/lanes.js'
 
 export default {
   name: 'Board',
   components: {
-    // RestaurantCard,
-    // NewRestaurant,
     Lane
   },
   data() {
     return {
-      restaurants: [],
-      addNewRestaurant: false
-    }
-  },
-  methods: {
-    addRestaurant(restaurant) {
-      this.restaurants.push(restaurant)
-      this.addNewRestaurant = false
-    },
-    removeRestaurant(removedRestaurant) {
-      this.restaurants = this.restaurants.filter(restaurant => restaurant.name !== removedRestaurant.name)
+      lanes: lanes
     }
   }
 }
@@ -41,10 +25,8 @@ export default {
 .board {
   display: flex;
   flex-direction: row;
+  justify-content: space-evenly;
   align-items: stretch;
-}
-
-.add-link {
-  cursor: pointer;
+  height: calc(100vh - 70px);
 }
 </style>
